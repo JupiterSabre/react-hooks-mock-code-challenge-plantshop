14e46151f682be4fde96d04a2a1f29bca731831c
Zulai Romero <zulairomero@gmail.com>
2023-08-09 11:12:04 -0400

    deliverables 1-3

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,18 +1,32 @@
 import React from "react";
+import { useState } from "react";
 
 function PlantCard({image, price, name}) {
+
+ const [inStock, setInStock] = useState(true)
+
+
   return (
     <li className="card">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+
+      <button onClick={() => 
+        {setInStock(!inStock)}} 
+        className={inStock ? "primary" : ""}> 
+      {inStock ? "In Stock" : "Out of Stock"}
+      </button>
+      
     </li>
   );
 }
 
 export default PlantCard;
+
+
+
+
+
+
+
